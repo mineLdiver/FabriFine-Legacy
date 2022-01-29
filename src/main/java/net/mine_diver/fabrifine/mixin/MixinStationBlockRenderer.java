ed650e83a5e2f0e4fd2ecce2ef74cefda8ff1cd6
mineLdiver <aabesedin@mail.ru>
2022-01-29 05:29:58 +0500

    Updated to the latest StAPI commit.

--- src/main/java/net/mine_diver/fabrifine/mixin/MixinStationBlockRenderer.java
+++ src/main/java/net/mine_diver/fabrifine/mixin/MixinStationBlockRenderer.java
@@ -3,7 +3,7 @@ package net.mine_diver.fabrifine.mixin;
 import net.mine_diver.fabrifine.render.OFTessellator;
 import net.minecraft.client.render.Tessellator;
 import net.modificationstation.stationapi.api.client.texture.atlas.Atlas;
-import net.modificationstation.stationapi.impl.client.texture.StationBlockRenderer;
+import net.modificationstation.stationapi.impl.client.texture.plugin.StationBlockRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,49 +16,49 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 public abstract class MixinStationBlockRenderer {
     // TODO: fast lighting better grass
 
-    @Inject(
-            method = "lambda$endMeshRender$0",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/render/Tessellator;draw()V",
-                    shift = At.Shift.AFTER
-            ),
-            locals = LocalCapture.CAPTURE_FAILHARD
-    )
-    private static void setRenderingInChunkFalse(Atlas atlas, CallbackInfo ci, Tessellator tessellator) {
-        OFTessellator.of(tessellator).setRenderingChunk(false);
-    }
-
-    @Redirect(
-            method = "lambda$endMeshRender$0",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/render/Tessellator;setOffset(DDD)V"
-            )
-    )
-    private static void stopOffset1(Tessellator instance, double d1, double d2, double v) {}
-
-    @Inject(
-            method = "prepareTessellator(Lnet/modificationstation/stationapi/api/client/texture/atlas/Atlas;)Lnet/minecraft/client/render/Tessellator;",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/render/Tessellator;start()V",
-                    ordinal = 0
-            ),
-            locals = LocalCapture.CAPTURE_FAILHARD
-    )
-    private void setRenderingInChunkTrue(Atlas atlas, CallbackInfoReturnable<Tessellator> cir, Tessellator tessellator) {
-        OFTessellator.of(tessellator).setRenderingChunk(true);
-    }
-
-    @Redirect(
-            method = "prepareTessellator(Lnet/modificationstation/stationapi/api/client/texture/atlas/Atlas;)Lnet/minecraft/client/render/Tessellator;",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/render/Tessellator;setOffset(DDD)V"
-            )
-    )
-    private void stopOffset2(Tessellator instance, double d1, double d2, double v) {}
+//    @Inject(
+//            method = "lambda$endMeshRender$0",
+//            at = @At(
+//                    value = "INVOKE",
+//                    target = "Lnet/minecraft/client/render/Tessellator;draw()V",
+//                    shift = At.Shift.AFTER
+//            ),
+//            locals = LocalCapture.CAPTURE_FAILHARD
+//    )
+//    private static void setRenderingInChunkFalse(Atlas atlas, CallbackInfo ci, Tessellator tessellator) {
+//        OFTessellator.of(tessellator).setRenderingChunk(false);
+//    }
+//
+//    @Redirect(
+//            method = "lambda$endMeshRender$0",
+//            at = @At(
+//                    value = "INVOKE",
+//                    target = "Lnet/minecraft/client/render/Tessellator;setOffset(DDD)V"
+//            )
+//    )
+//    private static void stopOffset1(Tessellator instance, double d1, double d2, double v) {}
+//
+//    @Inject(
+//            method = "prepareTessellator(Lnet/modificationstation/stationapi/api/client/texture/atlas/Atlas;)Lnet/minecraft/client/render/Tessellator;",
+//            at = @At(
+//                    value = "INVOKE",
+//                    target = "Lnet/minecraft/client/render/Tessellator;start()V",
+//                    ordinal = 0
+//            ),
+//            locals = LocalCapture.CAPTURE_FAILHARD
+//    )
+//    private void setRenderingInChunkTrue(Atlas atlas, CallbackInfoReturnable<Tessellator> cir, Tessellator tessellator) {
+//        OFTessellator.of(tessellator).setRenderingChunk(true);
+//    }
+//
+//    @Redirect(
+//            method = "prepareTessellator(Lnet/modificationstation/stationapi/api/client/texture/atlas/Atlas;)Lnet/minecraft/client/render/Tessellator;",
+//            at = @At(
+//                    value = "INVOKE",
+//                    target = "Lnet/minecraft/client/render/Tessellator;setOffset(DDD)V"
+//            )
+//    )
+//    private void stopOffset2(Tessellator instance, double d1, double d2, double v) {}
 
     //TODO: fix stuff below
 
